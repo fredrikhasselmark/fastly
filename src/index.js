787a7efbf627a88778b7d17337e448b656388e89
Fredrik Hasselmark <fredrik.hasselmark@gmail.com>
2024-07-26 00:36:11 +0200

    Add source code from Fiddle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-/// <reference types="@fastly/js-compute" />
+import { Router } from "@fastly/expressly";
 
-addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+const HOMEPAGE_ORIGIN = "origin_1";
+const STATUS_ORIGIN = "origin_0";
+const router = new Router();
 
-async function handleRequest(event) {
-  return new Response("OK", { status: 200 });
-}
+// If the URL begins with /status/
+router.get("/status/(.*)", async (req, res) => {
+  // Send the request to the status backend.
+  res.send(await fetch(req, {
+    backend: STATUS_ORIGIN
+  }));
+});
+
+router.get("/", async (req, res) => {
+  // Send the request to the homepage backend.
+  res.send(await fetch(req, {
+    backend: HOMEPAGE_ORIGIN
+  }));
+});
+
+router.all("(.*)", (req, res) => {
+  res.withStatus(404).send("The page you requested could not be found");
+});
+
+router.listen();
